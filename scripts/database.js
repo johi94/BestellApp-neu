5c1970c6ee030011fa1ee528f46f395bb8a86fb2
Jonas Hildebrand <jhildebrand94@googlemail.com>
2026-03-12 01:06:19 +0100

    Database db.js filled with pictures dishes and descriptions

--- scripts/database.js
+++ scripts/database.js
@@ -1,49 +1,58 @@
-let myDishes = [
+const myDishes = [
     {
-        "name" : "",
-        "price" : 8.90,
-        "description" : ""
+        "image" : "./assets/img/tacos_al_pastor_compressed.jpg",
+        "name" : "Tacos al Pastor",
+        "price" : 8.9,
+        "description" : "Marinated pork, topped with sweet pineapple, fresh cilantro, and chopped onion."
     },
       {
-        "name" : "",
-        "price" : 8.90,
-        "description" : ""
+        "image" : "./assets/img/tacos_asada_compressed.jpg",
+        "name" : "Tacos de Carne Asada",  
+        "price" : 8.9,
+        "description" : "Grilled, tender steak, topped with fresh cilantro and chopped onion."
     },
       {
-        "name" : "",
-        "price" : 8.90,
-        "description" : ""
+        "image" : "./assets/img/tacos_birria_compressed.jpg",
+        "name" : "Tacos de Birria",
+        "price" : 8.9,
+        "description" : "Slow-braised beef, topped with fresh cilantro and chopped onion."
     },
       {
-        "name" : "",
-        "price" : 8.90,
-        "description" : ""
+        "image" : "./assets/img/burrito_pollo_compressed.jpg",
+        "name" : "Burrito de Pollo",
+        "price" : 12.9,
+        "description" : "Grilled, seasoned chicken wrapped in a warm flour tortilla with rice, beans, and salsa."
     },
       {
-        "name" : "",
-        "price" : 8.90,
-        "description" : ""
+        "image" : "./assets/img/burrito_carne_asada_compressed.jpg",
+        "name" : "Burrito de carne Asada",
+        "price" : 12.9,
+        "description" : "Grilled, tender steak wrapped in a warm flour tortilla with rice, beans and salsa."
     },
       {
-        "name" : "",
-        "price" : 8.90,
-        "description" : ""
+        "image" : "./assets/img/burrito_birria_compressed.jpg",
+        "name" : "Burrito de Birria",
+        "price" : 12.9,
+        "description" : "Slow-braised beef wrapped in a warm flour tortilla with melted cheese."
     },
       {
-        "name" : "",
-        "price" : 8.90,
-        "description" : ""
+        "image" : "./assets/img/churros_compressed.jpg",
+        "name" : "Churros",
+        "price" : 4.9,
+        "description" : "Golden, crispy churros dusted with cinnamon sugar."
     },
       {
-        "name" : "",
-        "price" : 8.90,
-        "description" : ""
+        "image" : "./assets/img/chocolate_cake_compressed.jpg",
+        "name" : "Chocolate lava cake",
+        "price" : 4.9,
+        "description" : "Rich, moist chocolate cake layered with smooth chocolate frosting."
     },
-      {
-        "name" : "",
-        "price" : 8.90,
-        "description" : ""
+     {
+        "image" : "./assets/img/flan_al_horno_compressed.jpg",
+        "name" : "Flan al horno",
+        "price" : 4.9,
+        "description" : "Oven-baked Mexican flan with a silky smooth texture, rich vanilla flavor, and a golden caramel topping."
     }
 ];
 
-let myBasket = [];
+const myBasket = [];
