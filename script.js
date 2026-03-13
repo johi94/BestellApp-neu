86f9f4b3ec8f298d33db6484f19108d1d6000ee5
Jonas Hildebrand <jhildebrand94@googlemail.com>
2026-03-13 19:02:13 +0100

    descriptions changed in database.js renderMyDishes changed

--- script.js
+++ script.js
@@ -3,11 +3,30 @@
 // #start region render-function
 
 function renderMyDishes() {
-    let dishesRef = document.getElementById('content_taco');
-    dishesRef.innerHTML = "";
-    for (indexMyDishes = 0; indexMyDishes < myDishes.length; indexMyDishes++) {
-        dishesRef.innerHTML += getDishesTemplate(indexMyDishes)
+  let tacosRef = document.getElementById("content_taco");
+  let burritosRef = document.getElementById("content_burrito");
+  let dessertsRef = document.getElementById("content_dessert");
+
+  tacosRef.innerHTML = "";
+  burritosRef.innerHTML = "";
+  dessertsRef.innerHTML = "";
+
+  for (
+    let indexMyDishes = 0; indexMyDishes < myDishes.length; indexMyDishes++
+  ) {
+    const dish = myDishes[indexMyDishes];
+    const Dishtemplate = getDishesTemplate(indexMyDishes);
+
+    if (dish.category === "taco") {
+      tacosRef.innerHTML += Dishtemplate;
     }
-};
 
+    if (dish.category === "burrito") {
+      burritosRef.innerHTML += Dishtemplate;
+    }
 
+    if (dish.category === "dessert") {
+      dessertsRef.innerHTML += Dishtemplate;
+    }
+  }
+}
--- scripts/database.js
+++ scripts/database.js
@@ -1,53 +1,62 @@
 const myDishes = [
     {
+        "category": "taco",
         "image" : "./assets/img/tacos_al_pastor_compressed.jpg",
         "name" : "Tacos al Pastor",
         "price" : 8.9,
         "description" : "Marinated pork seasoned with dried chilies, spices, and pineapple, slow-roasted for a rich, savory flavor. Served on warm corn tortillas and topped with sweet pineapple, fresh cilantro, and chopped onion."
     },
-      {
+      { 
+        "category": "taco",
         "image" : "./assets/img/tacos_asada_compressed.jpg",
         "name" : "Tacos de Carne Asada",  
         "price" : 8.9,
         "description" : "Tender, grilled steak cooked over charcoal to enhance its smoky flavor. The meat is sliced thinly and seasoned with traditional spices. They are served on warm corn tortillas topped with fresh cilantro and chopped onion."
     },
-      {
+      { 
+        "category": "taco",
         "image" : "./assets/img/tacos_birria_compressed.jpg",
         "name" : "Tacos de Birria",
         "price" : 8.9,
         "description" : "Slow-braised lamb, simmered in a rich, flavorful chili broth to infuse it with deep, savory taste. The meat is shredded and placed inside warm corn tortillas, topped with fresh cilantro and chopped onion. Served with a side of rich birria broth for dipping."
     },
       {
+        "category": "burrito",
         "image" : "./assets/img/burrito_pollo_compressed.jpg",
         "name" : "Burrito de Pollo",
         "price" : 12.9,
         "description" : "A hearty flour tortilla filled with tender grilled chicken, seasoned with a blend of spices, and cooked to perfection. Inside, you'll find fluffy rice, savory refried beans, fresh shredded lettuce, diced tomatoes, and melted cheese. Topped with sour cream and salsa."
     },
       {
+        "category": "burrito",
         "image" : "./assets/img/burrito_carne_asada_compressed.jpg",
         "name" : "Burrito de carne Asada",
         "price" : 12.9,
         "description" : "A flavorful flour tortilla packed with tender, marinated carne asada grilled to perfection. Inside, you'll find fluffy rice, savory refried beans, fresh shredded lettuce, diced tomatoes, and melted cheese. Topped with sour cream and salsa."
     },
       {
+        "category": "burrito",
         "image" : "./assets/img/burrito_birria_compressed.jpg",
         "name" : "Burrito de Birria",
         "price" : 12.9,
         "description" : "A warm flour tortilla filled with tender, slow-braised birria lamb, seasoned with traditional spices. Inside, it includesfluffy rice, refried beans, fresh shredded lettuce, diced tomatoes, and melted cheese. Served with a side of rich birria broth for dipping."
     },
       {
+        "category": "dessert",
         "image" : "./assets/img/churros_compressed.jpg",
         "name" : "Churros",
         "price" : 4.9,
         "description" : "Crispy, golden-brown fried dough sticks dusted with cinnamon sugar, served warm. Accompanied by a rich, velvety chocolate dipping sauce for the perfect sweet treat."
     },
       {
+        "category": "dessert",
         "image" : "./assets/img/chocolate_cake_compressed.jpg",
         "name" : "Chocolate lava cake",
         "price" : 4.9,
         "description" : "A decadent, warm chocolate cake with a gooey, molten center bursting with rich, melted chocolate. Topped with fresh rasberry and blueberry, and drizzled with a luscious chocolate sauce for an irresistible fruit and chocolate combination."
     },
      {
+      "category": "dessert",
         "image" : "./assets/img/flan_al_horno_compressed.jpg",
         "name" : "Flan al horno",
         "price" : 4.9,
