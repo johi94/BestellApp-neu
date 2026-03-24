0a24cb14bc2f1cd59674b61225021f41c380a380
Jonas Hildebrand <jhildebrand94@googlemail.com>
2026-03-24 14:38:50 +0100

    database changed

--- scripts/database.js
+++ scripts/database.js
@@ -4,63 +4,63 @@ const myDishes = [
         "image" : "./assets/img/tacos_al_pastor_compressed.jpg",
         "name" : "Tacos al Pastor",
         "price" : 8.9,
-        "description" : "Marinated pork seasoned with dried chilies, spices, and pineapple, slow-roasted for a rich, savory flavor. Served on warm corn tortillas and topped with sweet pineapple, fresh cilantro, and chopped onion."
+        "description" : "Marinated pork seasoned with dried chilies, spices, and pineapple."
     },
       { 
         "category": "taco",
         "image" : "./assets/img/tacos_asada_compressed.jpg",
         "name" : "Tacos de Carne Asada",  
         "price" : 8.9,
-        "description" : "Tender, grilled steak cooked over charcoal to enhance its smoky flavor. The meat is sliced thinly and seasoned with traditional spices. They are served on warm corn tortillas topped with fresh cilantro and chopped onion."
+        "description" : "Tender, grilled steak cooked over charcoal to enhance its smoky flavor."
     },
       { 
         "category": "taco",
         "image" : "./assets/img/tacos_birria_compressed.jpg",
         "name" : "Tacos de Birria",
         "price" : 8.9,
-        "description" : "Slow-braised lamb, simmered in a rich, flavorful chili broth to infuse it with deep, savory taste. The meat is shredded and placed inside warm corn tortillas, topped with fresh cilantro and chopped onion. Served with a side of rich birria broth for dipping."
+        "description" : "Slow-braised lamb, simmered in a rich, flavorful chili broth to infuse it with deep, savory taste."
     },
       {
         "category": "burrito",
         "image" : "./assets/img/burrito_pollo_compressed.jpg",
         "name" : "Burrito de Pollo",
         "price" : 12.9,
-        "description" : "A hearty flour tortilla filled with tender grilled chicken, seasoned with a blend of spices, and cooked to perfection. Inside, you'll find fluffy rice, savory refried beans, fresh shredded lettuce, diced tomatoes, and melted cheese. Topped with sour cream and salsa."
+        "description" : "Flour tortilla filled with tender grilled chicken, fluffy rice, savory refried beans and melted cheese."
     },
       {
         "category": "burrito",
         "image" : "./assets/img/burrito_carne_asada_compressed.jpg",
         "name" : "Burrito de carne Asada",
         "price" : 12.9,
-        "description" : "A flavorful flour tortilla packed with tender, marinated carne asada grilled to perfection. Inside, you'll find fluffy rice, savory refried beans, fresh shredded lettuce, diced tomatoes, and melted cheese. Topped with sour cream and salsa."
+        "description" : "Flour tortilla packed with tender, marinated carne asada, fluffy rice, savory refried beans and melted cheese."
     },
       {
         "category": "burrito",
         "image" : "./assets/img/burrito_birria_compressed.jpg",
         "name" : "Burrito de Birria",
         "price" : 12.9,
-        "description" : "A warm flour tortilla filled with tender, slow-braised birria lamb, seasoned with traditional spices. Inside, it includesfluffy rice, refried beans, fresh shredded lettuce, diced tomatoes, and melted cheese. Served with a side of rich birria broth for dipping."
+        "description" : "Flour tortilla filled with tender, slow-braised birria lamb, fluffy rice, refried beans and melted cheese."
     },
       {
         "category": "dessert",
         "image" : "./assets/img/churros_compressed.jpg",
         "name" : "Churros",
         "price" : 4.9,
-        "description" : "Crispy, golden-brown fried dough sticks dusted with cinnamon sugar, served warm. Accompanied by a rich, velvety chocolate dipping sauce for the perfect sweet treat."
+        "description" : "Crispy, golden-brown fried dough sticks dusted with cinnamon sugar."
     },
       {
         "category": "dessert",
         "image" : "./assets/img/chocolate_cake_compressed.jpg",
         "name" : "Chocolate lava cake",
         "price" : 4.9,
-        "description" : "A decadent, warm chocolate cake with a gooey, molten center bursting with rich, melted chocolate. Topped with fresh rasberry and blueberry, and drizzled with a luscious chocolate sauce for an irresistible fruit and chocolate combination."
+        "description" : "Warm chocolate cake with a gooey, molten center bursting with rich, melted chocolate."
     },
      {
       "category": "dessert",
         "image" : "./assets/img/flan_al_horno_compressed.jpg",
         "name" : "Flan al horno",
         "price" : 4.9,
-        "description" : "A smooth, creamy caramel flan baked to perfection in the oven, with a delicate custard base infused with hints of vanilla. Topped with a rich caramel sauce that gently pools around the custard, creating a luscious and indulgent dessert."
+        "description" : "Smooth, creamy caramel flan baked to perfection in the oven."
     }
 ];
 
