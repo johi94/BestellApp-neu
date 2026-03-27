4566dc2bd291ca26b11c760e24feb84477e8bd3e
Jonas Hildebrand <jhildebrand94@googlemail.com>
2026-03-27 14:54:20 +0100

    template and index ordened

--- scripts/templates.js
+++ scripts/templates.js
@@ -11,23 +11,24 @@ function getDishesTemplate(indexMyDishes) {
         <div class="name_description">
          <h3>${dish.name}</h3>
           <p class="dish_description">${dish.description}</p>
-           </div>
-            <div class="price_btn_dishes">
-             <p class="dish_price">${dish.price.toFixed(2).replace(".", ",")} €</p>
+        </div>
+          <div class="price_btn_dishes">
+            <p class="dish_price">${dish.price.toFixed(2).replace(".", ",")} €</p>
              <div class="btn-container">
               <button class="add-to-basket-btn" onclick="dishToBasket(${indexMyDishes})">Add to basket
                </button>
                 ${count > 0 ? `<span class="dish-counter">${count}</span>` : ""}
-                 </div>
-                  </div>
-                   </div>
-                    </article>`;
+             </div>
+         </div>
+      </div>
+  </article>`;
 }
 
 function getBasketTemplate(indexMyBasket) {
   const basketdish = myBasket[indexMyBasket];
 
-  return /*html*/ `<article class="dishes_basket">
+  return /*html*/ `
+  <article class="dishes_basket">
    <h3 class="basket_dish_name">${basketdish.amount}x ${basketdish.name}</h3>
     <div class="basket_btns_price">
      <button onclick="removeDishFromBasket(${indexMyBasket})" class="basket_btn">-</button>
@@ -35,9 +36,9 @@ function getBasketTemplate(indexMyBasket) {
        <div class="basket_delete_price">
         <button onclick="deleteDishFromBasket(${indexMyBasket})" class="basket_delete_btn">&#x1F5D1</button> 
          <p class="basket_dish_price">${(basketdish.price * basketdish.amount).toFixed(2).replace(".", ",")} €</p>
-          </div>
-           </div>
-            </article>`;
+      </div>
+    </div>
+  </article>`;
 }
 
 function deliveryCosts() {
