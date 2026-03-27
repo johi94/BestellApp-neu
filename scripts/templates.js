5668cff7772714504671eb78a012a52c1bc43274
Jonas Hildebrand <jhildebrand94@googlemail.com>
2026-03-27 15:05:07 +0100

    template ordened

--- scripts/templates.js
+++ scripts/templates.js
@@ -5,40 +5,40 @@ function getDishesTemplate(indexMyDishes) {
   const count = myBasket.find((item) => item.name === dish.name)?.amount || 0;
 
   return /*html*/ `
-  <article class="dishes">
-    <img src="${dish.image}" alt="Cover von ${dish.name}" class="dish-img" />
-     <div class="space_between">
-      <div class="name_description">
-        <h3>${dish.name}</h3>
-        <p class="dish_description">${dish.description}</p>
+    <article class="dishes">
+     <img src="${dish.image}" alt="Cover von ${dish.name}" class="dish-img" />
+      <div class="space_between">
+        <div class="name_description">
+         <h3>${dish.name}</h3>
+         <p class="dish_description">${dish.description}</p>
+        </div>
+        <div class="price_btn_dishes">
+          <p class="dish_price">${dish.price.toFixed(2).replace(".", ",")} €</p>
+           <div class="btn-container">
+            <button class="add-to-basket-btn" onclick="dishToBasket(${indexMyDishes})">Add to basket
+            </button>
+            ${count > 0 ? `<span class="dish-counter">${count}</span>` : ""}
+           </div>
+        </div>
       </div>
-      <div class="price_btn_dishes">
-        <p class="dish_price">${dish.price.toFixed(2).replace(".", ",")} €</p>
-        <div class="btn-container">
-        <button class="add-to-basket-btn" onclick="dishToBasket(${indexMyDishes})">Add to basket
-        </button>
-        ${count > 0 ? `<span class="dish-counter">${count}</span>` : ""}
-      </div>
-      </div>
-    </div>
-  </article>`;
+   </article>`;
 }
 
 function getBasketTemplate(indexMyBasket) {
   const basketdish = myBasket[indexMyBasket];
 
   return /*html*/ `
-  <article class="dishes_basket">
-   <h3 class="basket_dish_name">${basketdish.amount}x ${basketdish.name}</h3>
-    <div class="basket_btns_price">
-     <button onclick="removeDishFromBasket(${indexMyBasket})" class="basket_btn">-</button>
-      <button onclick="addDishToBasket(${indexMyBasket})" class="basket_btn">+</button>
-       <div class="basket_delete_price">
-        <button onclick="deleteDishFromBasket(${indexMyBasket})" class="basket_delete_btn">&#x1F5D1</button> 
+   <article class="dishes_basket">
+    <h3 class="basket_dish_name">${basketdish.amount}x ${basketdish.name}</h3>
+     <div class="basket_btns_price">
+      <button onclick="removeDishFromBasket(${indexMyBasket})" class="basket_btn">-</button>
+       <button onclick="addDishToBasket(${indexMyBasket})" class="basket_btn">+</button>
+        <div class="basket_delete_price">
+         <button onclick="deleteDishFromBasket(${indexMyBasket})" class="basket_delete_btn">&#x1F5D1</button> 
          <p class="basket_dish_price">${(basketdish.price * basketdish.amount).toFixed(2).replace(".", ",")} €</p>
-      </div>
-    </div>
-  </article>`;
+       </div>
+     </div>
+   </article>`;
 }
 
 function deliveryCosts() {
